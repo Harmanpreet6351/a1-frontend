243aad8bb49cbd3e383e9bedc2544b4c21cc8dbf
Harmanpreet Singh <harmanpreet181001@gmail.com>
2024-12-02 02:49:25 +0530

    COMMIT

--- src/components/home/our-services-components.tsx
+++ src/components/home/our-services-components.tsx
@@ -1,6 +1,11 @@
-import { HTMLMotionProps, motion } from "framer-motion"
-import Image from "next/image"
-import Viking from "@/assets/hero-backgroun.png"
+import { HTMLMotionProps, motion } from "framer-motion";
+import Image from "next/image";
+import Viking from "@/assets/hero-backgroun.png";
+import MinorRepair from "@/assets/our-services/minor-repairs.jpg";
+import CustomCabinets from "@/assets/our-services/custom-cabinets.jpg"
+import Electircal from "@/assets/our-services/electrical.jpg"
+import Furniture from "@/assets/our-services/furniture.jpg"
+import Plumbing from "@/assets/our-services/plumbing.jpg"
 
 const commonLeftAnimations: HTMLMotionProps<"div"> = {
   animate: {
@@ -13,9 +18,9 @@ const commonLeftAnimations: HTMLMotionProps<"div"> = {
     opacity: 0,
   },
   transition: {
-    duration: 0.5
-  }
-}
+    duration: 0.5,
+  },
+};
 
 const commonRightAnimations: HTMLMotionProps<"div"> = {
   animate: {
@@ -28,67 +33,105 @@ const commonRightAnimations: HTMLMotionProps<"div"> = {
     opacity: 0,
   },
   transition: {
-    duration: 0.5
-  }
-}
+    duration: 0.5,
+  },
+};
 
 export const AppliancesComponent = () => {
   return (
     <>
-      <motion.div key="washing-machine-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>
+      <motion.div
+        key="washing-machine-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
         <h1 className="font-bold text-[24px] mb-4">Appliance Installations</h1>
         <ul className="list-disc ml-5">
           <li>Dishwasher</li>
           <li>Wall Ovens</li>
           <li>Rangehood / Chimney Hood Fans with Ventilation</li>
           <li>OTR / Microwave</li>
-          <li>Built-in Fridges and Freezers<br />(Specializes in Luxury Appliance)</li>
+          <li>
+            Built-in Fridges and Freezers
+            <br />
+            (Specializes in Luxury Appliance)
+          </li>
           <li>Door Panels</li>
           <li>Washer and Dryers with Side Venting</li>
           <li>Cooktops</li>
         </ul>
       </motion.div>
-      <motion.div key="washing-machine-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image src={Viking} alt="viking" style={{boxShadow: "2px 2px 8px gray"}} />
+      <motion.div
+        key="washing-machine-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image
+          src={Viking}
+          alt="viking"
+          style={{ boxShadow: "2px 2px 8px gray" }}
+        />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export const KitchenComponent = () => {
+export const MinorRepairsComponent = () => {
   return (
     <>
-      <motion.div key="kitchen-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>lorem ipsum lorem ipsum lorem ipsum</motion.div>
-      <motion.div key="kitchen-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image className="shadow-2xl" src={Viking} alt="viking" />
+      <motion.div
+        key="kitchen-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
+        lorem ipsum lorem ipsum lorem ipsum
+      </motion.div>
+      <motion.div
+        key="kitchen-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image className="shadow-2xl" src={MinorRepair} alt="viking" />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export const PlumbingComponent = () => {
   return (
     <>
-      <motion.div key="plumbing-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>
-      <h1 className="font-bold text-[24px] mb-4">Plumbing</h1>
-      <ul className="list-disc ml-5">
-        <li>Kitchen and Bathroom Sinks and Faucets</li>
-        <li>Toilets</li>
-        <li>Garburator</li>
-        <li>Vanities</li>
-      </ul>
+      <motion.div
+        key="plumbing-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
+        <h1 className="font-bold text-[24px] mb-4">Plumbing</h1>
+        <ul className="list-disc ml-5">
+          <li>Kitchen and Bathroom Sinks and Faucets</li>
+          <li>Toilets</li>
+          <li>Garburator</li>
+          <li>Vanities</li>
+        </ul>
       </motion.div>
-      <motion.div key="plumbing-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image className="shadow-2xl" src={Viking} alt="viking" />
+      <motion.div
+        key="plumbing-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image className="shadow-2xl" src={Plumbing} alt="viking" />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export const ElectricalComponent = () => {
   return (
     <>
-      <motion.div key="electrical-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>
+      <motion.div
+        key="electrical-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
         <h1 className="font-bold text-[24px] mb-4">Electrical</h1>
         <ul className="list-disc ml-5">
           <li>Light Fixtures</li>
@@ -98,17 +141,25 @@ export const ElectricalComponent = () => {
           <li>On Demand festive lighting and decoration</li>
         </ul>
       </motion.div>
-      <motion.div key="electrical-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image className="shadow-2xl" src={Viking} alt="viking" />
+      <motion.div
+        key="electrical-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image className="shadow-2xl" src={Electircal} alt="viking" />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export const FurnitureComponent = () => {
   return (
     <>
-      <motion.div key="furniture-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>
+      <motion.div
+        key="furniture-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
         <h1 className="font-bold text-[24px] mb-4">Furniture</h1>
         <ul className="list-disc ml-5">
           <li>Bed Assembly</li>
@@ -120,25 +171,44 @@ export const FurnitureComponent = () => {
           <li>All Kinds of Appliance Door Panels</li>
         </ul>
       </motion.div>
-      <motion.div key="furniture-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image className="shadow-2xl" src={Viking} alt="viking" />
+      <motion.div
+        key="furniture-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image className="shadow-2xl" src={Furniture} alt="viking" />
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export const CustomCabinetsComponent = () => {
   return (
     <>
-      <motion.div key="painting-left" className="w-full lg:w-1/2" {...commonLeftAnimations}>
+      <motion.div
+        key="painting-left"
+        className="w-full lg:w-1/2"
+        {...commonLeftAnimations}
+      >
         <h1 className="font-bold text-[24px] mb-4">Custom Cabinets</h1>
         <ul className="list-disc ml-5">
-         <li>We offer expert design, build, and installation services for custom cabinets tailored specifically to enhance the functionality and aesthetics of your kitchen, bathroom, and den area. Our team works closely with you to create personalized cabinetry solutions that meet your unique needs, using high-quality materials and precise craftsmanship to ensure a perfect fit in every room of your home.</li>
+          <li>
+            We offer expert design, build, and installation services for custom
+            cabinets tailored specifically to enhance the functionality and
+            aesthetics of your kitchen, bathroom, and den area. Our team works
+            closely with you to create personalized cabinetry solutions that
+            meet your unique needs, using high-quality materials and precise
+            craftsmanship to ensure a perfect fit in every room of your home.
+          </li>
         </ul>
       </motion.div>
-      <motion.div key="painting-right" className="w-full lg:w-1/2" {...commonRightAnimations}>
-        <Image className="shadow-2xl" src={Viking} alt="viking" />
+      <motion.div
+        key="painting-right"
+        className="w-full lg:w-1/2"
+        {...commonRightAnimations}
+      >
+        <Image className="shadow-2xl" src={CustomCabinets} alt="viking" />
       </motion.div>
     </>
-  )
-}
+  );
+};
--- src/components/home/our-services-section.tsx
+++ src/components/home/our-services-section.tsx
@@ -1,81 +1,141 @@
-"use client"
+"use client";
 
-import { AnimatePresence } from "framer-motion"
-import { useEffect, useMemo, useState } from "react"
-import { AppliancesComponent, CustomCabinetsComponent, ElectricalComponent, FurnitureComponent, KitchenComponent, PlumbingComponent, } from "./our-services-components"
-import Image from "next/image"
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useMemo, useState } from "react";
+import {
+  AppliancesComponent,
+  CustomCabinetsComponent,
+  ElectricalComponent,
+  FurnitureComponent,
+  MinorRepairsComponent,
+  PlumbingComponent,
+} from "./our-services-components";
+import Image from "next/image";
 
-
-import PaintingIcon from "@/assets/our-services/painting.svg"
-import WashingMachine from "@/assets/our-services/washing-machine.svg"
-import Kitchen from "@/assets/our-services/kitchen.svg"
-import Plumbing from "@/assets/our-services/plumbing.svg"
-import Electrical from "@/assets/our-services/electrical.svg"
-import Furniture from "@/assets/our-services/furniture.svg"
-import Repair from "@/assets/our-services/repair.svg"
-import Cabinets from "@/assets/our-services/cabinets.svg"
+import PaintingIcon from "@/assets/our-services/painting.svg";
+import WashingMachine from "@/assets/our-services/washing-machine.svg";
+import Kitchen from "@/assets/our-services/kitchen.svg";
+import Plumbing from "@/assets/our-services/plumbing.svg";
+import Electrical from "@/assets/our-services/electrical.svg";
+import Furniture from "@/assets/our-services/furniture.svg";
+import Repair from "@/assets/our-services/repair.svg";
+import Cabinets from "@/assets/our-services/cabinets.svg";
 
 export default function OurServicesSection() {
-
-  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(true)
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(true);
 
   useEffect(() => {
-    setIsSmallScreen(window.innerWidth < 768)
-  }, [])
+    setIsSmallScreen(window.innerWidth < 768);
+  }, []);
 
-  const dimension = useMemo(() => isSmallScreen ? 40 : 60, [isSmallScreen])
+  const dimension = useMemo(() => (isSmallScreen ? 40 : 60), [isSmallScreen]);
 
-  const [itemName, setItemName] = useState<string>("cabinet")
+  const [itemName, setItemName] = useState<string>("cabinet");
 
-  const buttonClassNames = useMemo(() => "shadow-md py-8 h-[90px] w-[90px] lg:h-[120px] lg:w-[120px] flex flex-col items-center justify-center", [])
+  const buttonClassNames = useMemo(
+    () =>
+      "shadow-md py-8 h-[90px] w-[90px] lg:h-[120px] lg:w-[120px] flex flex-col items-center justify-center",
+    [],
+  );
 
   return (
     <div id="services" className="py-16 lg:px-32 md:px-16 px-4">
-      <h1 className="text-center text-[32px] lg:text-[42px] font-bold">Our Services</h1>
+      <h1 className="text-center text-[32px] lg:text-[42px] font-bold">
+        Our Services
+      </h1>
       <div className="grid grid-cols-3 gap-5 md:grid-cols-6 pt-16 pb-20 place-items-center place-content-center">
-        <button className={buttonClassNames} onClick={() => setItemName("painting")}>
-          <Image src={Cabinets} width={dimension} height={dimension} alt="Painting" className="mt-2" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("painting")}
+        >
+          <Image
+            src={Cabinets}
+            width={dimension}
+            height={dimension}
+            alt="Painting"
+            className="mt-2"
+          />
           <p className="text-xs font-semibold pt-2">Custom Cabinets</p>
         </button>
-        <button className={buttonClassNames} onClick={() => setItemName("appliances")}>
-          <Image src={WashingMachine} width={dimension} height={dimension} alt="Painting" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("appliances")}
+        >
+          <Image
+            src={WashingMachine}
+            width={dimension}
+            height={dimension}
+            alt="Painting"
+          />
           <p className="text-xs font-semibold">Appliances</p>
         </button>
-        <button className={buttonClassNames} onClick={() => setItemName("plumbing")}>
-          <Image src={Plumbing} width={dimension} height={dimension} alt="Painting" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("plumbing")}
+        >
+          <Image
+            src={Plumbing}
+            width={dimension}
+            height={dimension}
+            alt="Painting"
+          />
           <p className="text-xs font-semibold">Plumbing</p>
         </button>
-        <button className={buttonClassNames} onClick={() => setItemName("electrical")}>
-          <Image src={Electrical} width={dimension} height={dimension} alt="Painting" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("electrical")}
+        >
+          <Image
+            src={Electrical}
+            width={dimension}
+            height={dimension}
+            alt="Painting"
+          />
           <p className="text-xs font-semibold pt-2">Electrical</p>
         </button>
-        <button className={buttonClassNames} onClick={() => setItemName("furniture")}>
-          <Image className="mt-3.5" src={Furniture} width={isSmallScreen ? 30 : 40} height={isSmallScreen ? 30 : 40} alt="Painting" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("furniture")}
+        >
+          <Image
+            className="mt-3.5"
+            src={Furniture}
+            width={isSmallScreen ? 30 : 40}
+            height={isSmallScreen ? 30 : 40}
+            alt="Painting"
+          />
           <p className="text-xs font-semibold">Furniture</p>
         </button>
-        <button className={buttonClassNames} onClick={() => setItemName("repair")}>
-          <Image src={Repair} width={dimension} height={isSmallScreen ? 30 : 40} alt="Painting" />
+        <button
+          className={buttonClassNames}
+          onClick={() => setItemName("repair")}
+        >
+          <Image
+            src={Repair}
+            width={dimension}
+            height={isSmallScreen ? 30 : 40}
+            alt="Painting"
+          />
           <p className="text-xs font-semibold">Minor Repairs</p>
         </button>
       </div>
-        <div className="flex flex-col-reverse gap-6 lg:gap-16 lg:flex-row justify-evenly items-center lg:items-start">
-          <AnimatePresence mode="wait" initial={false}>
-          {
-            itemName === "repair" ? <KitchenComponent />
-            :
-            itemName === "appliances" ? <AppliancesComponent /> 
-            :
-            itemName === "plumbing" ? <PlumbingComponent />
-            :
-            itemName === "electrical" ? <ElectricalComponent />
-            :
-            itemName === "furniture" ? <FurnitureComponent />
-            :
-            itemName === "painting" ? <CustomCabinetsComponent />
-            : null
-          }
-          </AnimatePresence>
-        </div>
+      <div className="flex flex-col-reverse gap-6 lg:gap-16 lg:flex-row justify-evenly items-center lg:items-start">
+        <AnimatePresence mode="wait" initial={false}>
+          {itemName === "repair" ? (
+            <MinorRepairsComponent />
+          ) : itemName === "appliances" ? (
+            <AppliancesComponent />
+          ) : itemName === "plumbing" ? (
+            <PlumbingComponent />
+          ) : itemName === "electrical" ? (
+            <ElectricalComponent />
+          ) : itemName === "furniture" ? (
+            <FurnitureComponent />
+          ) : itemName === "painting" ? (
+            <CustomCabinetsComponent />
+          ) : null}
+        </AnimatePresence>
+      </div>
     </div>
-  )
+  );
 }
